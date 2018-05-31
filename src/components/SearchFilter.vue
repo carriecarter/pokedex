d676097c03028d0281bb914bf55588fb7560b68d
KLPainter <KLPainter@gmail.com>
2018-05-31 15:19:21 -0700

    'working dropdown'

--- src/components/SearchFilter.vue
+++ src/components/SearchFilter.vue
@@ -1,20 +1,29 @@
 <template>
   <div>
-    <h1>SearchFilter</h1>
-    
+    <h1>Select your Pokemon's Qualities</h1>
+    <select v-model="selected">
+      <option v-for="pokemon in pokemonData"
+        v-bind:key="pokemon.id" 
+        v-bind:value="pokemon.type_1">
+        {{ pokemon.type_1 }}
+      </option>
+    </select>
+    <span>Selected: {{ selected }}</span>
   </div>
 </template>
 
 <script>
 
+import pokemonData from '../pokemon.js'
 
 export default {
   data() {
-    return {
-      name: 'searchfilter'
-    }
-  }
-}
+    return { 
+      pokemonData,
+      selected: ''
+    };
+  },
+};
 
 </script>
 
