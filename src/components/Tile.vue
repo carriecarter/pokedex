76b509287336d95dcee98eeda797561c1fb8ecda
carriecarter <carrie@carrie-carter.com>
2018-06-04 01:17:08 -0700

    heading to bed with card structure in place

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="tile" :class="pokemon.type_1">
-    <h3>{{ pokemon.pokemon }}</h3>
-    <p><strong>Primary Type: </strong>{{ pokemon.type_1 }}</p>
-    <p><strong>Secondary Type: </strong>{{ pokemon.type_2 }}</p>
-    <p><strong>Speed: </strong>{{ pokemon.speed }}</p>
-    <p><strong>Attack: </strong>{{ pokemon.attack }}</p>
-    <p><strong>Defense: </strong>{{ pokemon.defense }}</p>
-    <img :src="pokemon.url_image">
+    <div class="container">
+      <img :src="pokemon.url_image" style="width:100%">
+      <h3>{{ pokemon.pokemon }}</h3>
+      <p><strong>Primary Type: </strong>{{ pokemon.type_1 }}</p>
+      <p><strong>Secondary Type: </strong>{{ pokemon.type_2 }}</p>
+      <p><strong>Speed: </strong>{{ pokemon.speed }}</p>
+      <p><strong>Attack: </strong>{{ pokemon.attack }}</p>
+      <p><strong>Defense: </strong>{{ pokemon.defense }}</p>
+   
+    </div>
   </div>
 </template>
 
@@ -18,21 +21,29 @@ export default {
 
 <style>
 
-.grass {
-  background-color: green;
+.tile {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    width: 20%;
+    border-radius: 5px;
+
 }
 
-.tile {
-  max-width: 400px;
-  padding: 6px;
-  margin: 10px;
-  border-radius: 20px;
-  border: solid 4px grey;
-  display: inline-block;
+.tile:hover {
+    box-shadow: 0 80px 400px 0 rgba(0,0,0,0.2);
 }
 
 img {
-  max-width: 100px;
-  /* float: right; */
+    border-radius: 5px 5px 5px 5px;
+    max-width: 200px;
+    align-self: top center;
 }
+
+.container {
+    padding: 2px 16px;
+    display: flex;
+    flex-direction:row;
+}
+
+
 </style>
